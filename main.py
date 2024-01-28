732d59f8178762744f5388db23148e42cf2f426a
Deepthikaa <150465903+Deepx999@users.noreply.github.com>
2024-01-28 12:59:48 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# 1st input: enter height in meters e.g: 1.65
+height = input()
+# 2nd input: enter weight in kilograms e.g: 72
+weight = input()
+w=int(weight)
+h=float(height)
+bmi=w / h ** 2
+BMI=int(bmi)
+print(BMI)
